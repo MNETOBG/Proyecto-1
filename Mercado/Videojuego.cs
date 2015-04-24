5c145915c3382f17e2b1915ba2d92e9e4ce32ff3
Mauricio Bautista <m.netobg@hotmail.com>
2015-04-24 10:29:29 -0700

    Se agrego la clase Videojuego

---
+++ Mercado/Videojuego.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mercado
+{
+	public class Videojuego : Articulo
+	{
+		public Videojuego ()
+		{
+		}
+
+		public Videojuego (string nombre, string descripcion, int codigo, double precio) : base (nombre, descripcion, codigo, precio)
+		{
+
+		}
+
+		public override double Descuento ()
+		{
+			return ((precio * .25) - precio);
+		}
+
+		public override string ToString()
+		{
+			return "El videojuego " + getNombre () + " con descripcion " + getDescripcion () + ", codigo " + getCodigo () + " y precio de " + Descuento() + " fue comprado. \n";
+		}
+		
+		//nuevo
+		public override void Comprar()
+		{
+			Console.WriteLine ( "Compro: " + getNombre () + " con descripcion: " + getDescripcion () + " codigo " + getCodigo () + " y precio " + Descuento () + ".\n");
+		}
+	}
+}
+
