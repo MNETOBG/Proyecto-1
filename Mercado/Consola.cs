c054c8e7a2c45b41d51110f123b3bc6eec1d24f8
Mauricio Bautista <m.netobg@hotmail.com>
2015-04-24 10:28:55 -0700

    Se agrego la clase Consola

---
+++ Mercado/Consola.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mercado
+{
+	public class Consola : Articulo
+	{
+		public Consola ()
+		{
+		}
+
+		public Consola (string nombre, string descripcion, int codigo, double precio) : base (nombre, descripcion, codigo, precio)
+		{
+
+		}
+
+		public override double Descuento ()
+		{
+			return ((precio * .10) - precio);
+		}
+
+		public override string ToString()
+		{
+			return "La consola " + getNombre () + " con descripcion " + getDescripcion () + ", codigo " + getCodigo () + " y precio de " + Descuento () + " fue comprado. \n";
+		}
+
+		//Nuevo
+		public override void Comprar()
+		{
+			Console.WriteLine ( "Compro: " + getNombre () + " con descripcion: " + getDescripcion () + " codigo " + getCodigo () + " y precio " + Descuento () + ".\n");
+		}
+	}
+}
+
