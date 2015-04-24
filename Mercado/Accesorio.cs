7d8b34562330ca7d09f2882cf9137044ad82cd08
Mauricio Bautista <m.netobg@hotmail.com>
2015-04-24 10:26:35 -0700

    Se agrego la clase Accesorio

---
+++ Mercado/Accesorio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mercado
+{
+	public class Accesorio : Articulo
+	{
+		public Accesorio ()
+		{
+		}
+
+		public Accesorio (string nombre, string descripcion, int codigo, double precio) : base (nombre, descripcion, codigo, precio)
+		{
+			
+		}
+
+		public override double Descuento ()
+		{
+			return ((precio * .20) - precio);
+		}
+
+		public override string ToString()
+		{
+			return "El accesorio " + getNombre () + " con descripcion " + getDescripcion () + ", codigo " + getCodigo () + " y precio de " + Descuento () + " fue comprado. \n"; 
+		}
+
+		//nuevo
+		public override void Comprar()
+		{
+			Console.WriteLine ( "Compro: " + getNombre () + " con descripcion: " + getDescripcion () + " codigo " + getCodigo () + " y precio " + Descuento () + ".\n");
+		}
+	}
+}
+
